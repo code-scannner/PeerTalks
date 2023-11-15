341bb4fe2fe54eb3e20b9778a18cfd5450a5cc31
code-scanner <cyber.gupta001@gmail.com>
2023-11-15 23:14:19 +0530

    changing bg

--- components/main/Chat/ChatBox.js
+++ components/main/Chat/ChatBox.js
@@ -19,25 +19,64 @@ const messages = [
         is_sender: true,
         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
     },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: false,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: false,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: false,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: false,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
+    {
+        is_sender: true,
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor magna vitae fermentum ultricies. Aliquam nec tellus id nisl imperdiet auctor quis in magna."
+    },
 ]
 
 export default function ChatBox() {
     return (
         <div
-        style={{
-            background:`url("image/chatbg.jpg")`,
-            backgroundSize:"60% 60%",
-            // backgroundPosition:"contain",
-            // backgroundBlendMode:"color-dodge"
-            
-        }}
-        className="relative text-[0.93rem] w-full p-6 overflow-y-auto h-[80vh] bg-primary-100">
-            
+            className="relative text-[0.93rem] w-full p-6 overflow-y-auto h-[80vh] bg-primary-50">
+            <div className="absolute inset-0 opacity-10 h-full"
+                style={{
+                    background: `url("image/chatbg.jpg")`,
+                    backgroundSize: "50%"
+                }}
+            />
+
             <ul className="flex flex-col">
-                {messages.map((elem, key,arr) => {
+                {messages.map((elem, key, arr) => {
                     return (
                         <li key={key} className={`${arr[key - 1] && arr[key - 1].is_sender != elem.is_sender ? "mt-4" : "mt-0.5"} flex ${elem.is_sender ? "justify-end" : "justify-start"}`}>
-                            <div className={`relative max-w-xl px-4 py-2 ${elem.is_sender ? "bg-primary-600 text-white rounded-s-xl rounded-e-md" : "text-gray-700 bg-white rounded-e-xl rounded-s-md"} shadow-chat`}>
+                            <div className={`relative max-w-xl px-4 py-2 ${elem.is_sender ? "bg-primary-600/90 text-white rounded-s-xl rounded-e-md" : "text-gray-700 bg-white/90 rounded-e-xl rounded-s-md"} shadow-chat`}>
                                 <span className="block">{elem.message}</span>
                             </div>
                         </li>
