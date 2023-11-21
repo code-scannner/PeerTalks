0c55e87a700505050c372777f9f68fcb9a2ebde5
code-scanner <cyber.gupta001@gmail.com>
2023-11-22 01:02:01 +0530

    chatbox bg

--- components/main/Chat/ChatBox.js
+++ components/main/Chat/ChatBox.js
@@ -70,13 +70,92 @@ const messages = [
     {
         is_sender: true,
         message: "See ya, Alice! Take care."
-    }
+    },
+    {
+        is_sender: false,
+        message: "Hey Bob! How's it going?",
+    },
+    {
+        is_sender: true,
+        message: "Hi Alice! I'm good, thanks. Just finished a workout. How about you?"
+    },
+    {
+        is_sender: false,
+        message: "Nice! I'm doing well too. Just got back from a coffee shop. ☕️"
+    },
+    {
+        is_sender: true,
+        message: "Sounds great! Anything exciting happening today?"
+    },
+    {
+        is_sender: false,
+        message: "Not much, just catching up on some work. What about you?"
+    },
+    {
+        is_sender: true,
+        message: "Same here, trying to get ahead on some projects. By the way, have you watched that new movie everyone's talking about?"
+    },
+    {
+        is_sender: false,
+        message: "Not yet! Is it worth watching?"
+    },
+    {
+        is_sender: true,
+        message: "Definitely! We should plan a movie night. How about this weekend?"
+    },
+    {
+        is_sender: false,
+        message: "Sounds like a plan! I'm in. What's the movie called?"
+    },
+    {
+        is_sender: true,
+        message: "It's called 'Beyond the Stars'. Heard the storyline is amazing."
+    },
+    {
+        is_sender: false,
+        message: "Awesome! I'll check it out. Can't wait for the weekend now. 😄"
+    },
+    {
+        is_sender: true,
+        message: "Me neither! It's a date then. Anything else on your mind?"
+    },
+    {
+        is_sender: false,
+        message: "Nope, just looking forward to the weekend. Anything specific you want to do?"
+    },
+    {
+        is_sender: true,
+        message: "Maybe grab dinner before the movie? I heard there's a new Italian place downtown."
+    },
+    {
+        is_sender: false,
+        message: "Perfect! Italian sounds great. Let's do it."
+    },
+    {
+        is_sender: true,
+        message: "Great! Looking forward to it. Text me if anything comes up."
+    },
+    {
+        is_sender: false,
+        message: "Will do! Catch you later, Bob."
+    },
+    {
+        is_sender: true,
+        message: "See ya, Alice! Take care."
+    },
 ]
 
 export default function ChatBox() {
     return (
         <div
-            className="relative text-[0.93rem] w-full p-6 overflow-y-auto h-[80vh] bg-primary-50">
+            className="relative bg-primary-50 text-[0.93rem] w-full p-6 overflow-y-auto h-[80vh]"
+            style={{
+                background: `url("image/chatbg.jpg")`,
+                backgroundColor:"hsl(278 100% 95%)",
+                backgroundSize: "50%",
+                backgroundBlendMode: "screen"
+            }}
+            >
             {/* <div className="absolute inset-0 opacity-20 h-full"
                 style={{
                     background: `url("image/chatbg.jpg")`,
