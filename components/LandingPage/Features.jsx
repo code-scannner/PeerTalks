9fd1f153d14e80463807cb39d3fbe937a496187e
code-scanner <cyber.gupta001@gmail.com>
2023-11-22 12:31:03 +0530

    resolved merge conflict

--- components/LandingPage/Features.jsx
+++ components/LandingPage/Features.jsx
@@ -1,22 +1,31 @@
+import React from "react";
 import FeatureCard from "./FeatureCard";
-import { MdNotifications } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { BsChatLeftText } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 const features = [
   {
     heading: "Personalized Profiles",
     description: "Safe sign-up, customizable profiles, and privacy controls for user accounts.",
-    icon: <MdNotifications fontSize={"10rem"} />
+    // icon: <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    //   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+    // </svg>
+    icon: <FaUser size={50}/>
   },
   {
     heading: "Messaging Features",
     description: "Instant conversations with emojis and reactions for real-time messaging.",
-    icon: <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-    </svg>
+    // icon: <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    //   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+    // </svg>
+    icon: <BsChatLeftText size={50}/> 
   },
   {
     heading: "Notifications and Alerts",
     description: "Push notifications, customizable preferences, and in-app alerts for timely notifications.",
-    icon: <MdNotifications fontSize={"10rem"} />
+    icon: <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+    </svg>
   },
   {
     heading: "Privacy and Security",
