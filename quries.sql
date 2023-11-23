78e9e8fda56d64245607758876856681413954e8
code-scanner <cyber.gupta001@gmail.com>
2023-11-24 01:26:00 +0530

    database queries

---
+++ quries.sql
@@ -0,0 +1,32 @@
+create database PeerTalks;
+
+use PeerTalks;
+
+desc Users;
+
+create table users(
+    username varchar(20) primary key,
+    password varchar(20),
+    fname varchar(20),
+    lname varchar(20),
+    bio varchar(255),
+    gender enum('Male','Female', 'Other'),
+    pic varchar(255),
+    lastLogin date,
+    regDate date
+);
+
+select * from users;
+
+insert into users
+values(
+	"utkarshtri",
+    "root",
+    "Utkarsh",
+    "Trivedi",
+    "I am a good web designer",
+    "Male",
+    "https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg",
+    curdate(),
+    curdate()
+);
