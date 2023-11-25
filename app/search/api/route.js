1037ade8d994c02fd1141bccb8244ef1801c84ad
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2023-11-25 16:31:01 +0530

    query

--- app/search/api/route.js
+++ app/search/api/route.js
@@ -2,9 +2,9 @@ import { urltoParams } from '@/lib/utils'
 import executeQuery from '../../../lib/db'
 
 export async function GET(req, res) {
-    const {search} = urltoParams(req.url);
+    const {username,search} = urltoParams(req.url);
     const response = await executeQuery({
-        query:`SELECT * FROM USERS WHERE USERNAME LIKE "%${search}%" LIMIT 30`
+        query:`SELECT * FROM USERS WHERE USERNAME LIKE "%${search}%" AND USERNAME<>"${username}"`
     })
     
     return Response.json({users : response});
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import axios from "axios";
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Search() {
     const [users, setUsers] = useState([]);
